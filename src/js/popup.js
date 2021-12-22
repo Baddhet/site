85778a1bc428f63a7879d4b0eeddd5f2fb076ef2
Baddhet <tenjibiu@gmail.com>
2021-12-22 21:16:55 +0300

    add popup

---
+++ src/js/popup.js
@@ -0,0 +1,15 @@
+const priceButton = document.querySelector('.price');
+const popup = document.querySelector('.popup');
+const shadow = document.querySelector('.shadow');
+
+priceButton.addEventListener('click', () => {
+  document.body.classList.add('not-sroll');
+  popup.classList.add('visible');
+  shadow.classList.add('visible');
+});
+
+shadow.addEventListener('click', () => {
+  document.body.classList.remove('not-sroll');
+  popup.classList.remove('visible');
+  shadow.classList.remove('visible');
+});
